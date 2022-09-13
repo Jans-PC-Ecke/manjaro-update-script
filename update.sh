918537990fa21bde3af6b6d28dce024190caf500
Jan <112738312+Jans-PC-Ecke@users.noreply.github.com>
2022-09-13 14:23:59 +0200

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd /etc
+
+# Teste, ob der Rechner arch-based ist
+if [ -d /etc/pacman.d ]
+	then 
+	sudo pacman -Syu && 
+	pacman -Rns $(pacman -Qtdq) &&
+	pacman -Sc
+	snap refresh && 
+	flatpak update -yy
+
+fi
+
+#Teste, ob der Rechner debian-basiert ist
+if [ -d /etc/apt ]
+	then 
+	sudo apt update &&
+	 apt upgrade &&
+	 sudo apt dist-upgrade &&
+	 flatpak update &&
+	 snap refresh && 
+	autoremove -yy
+fi
